2a15c139c1b4e9e221427033f1a634d7d58ee708
pkmkamonji <sp2tpkamonji@gmail.com>
2025-10-04 02:37:34 +0800

    uncomment print

--- main.js
+++ main.js
@@ -152,7 +152,7 @@ const main = async () => {
                                 console.log(respAntrean)
 
                                 // print antrean
-                                // await PRINT_ANTREAN({ ...respAntrean, from: 'Mobile JKN', patient: { name: nikData.data.nama, nik: nikData.data.nik, address: nikData.data.alamat_ktp } });
+                                await PRINT_ANTREAN({ ...respAntrean, from: 'Mobile JKN', patient: { name: nikData.data.nama, nik: nikData.data.nik, address: nikData.data.alamat_ktp } });
 
                             }
                         }
